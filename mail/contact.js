ff40b7380db7a72ac16421453102ac8117b821ad
moiz2194 <moiz23032194@gmail.com>
2022-12-19 06:24:02 +0500

    sen

--- mail/contact.js
+++ mail/contact.js
@@ -13,43 +13,52 @@ $(function () {
 
             $this = $("#sendMessageButton");
             $this.prop("disabled", true);
+            Email.send({
+                SecureToken : "a0b85244-b42c-4004-8add-11653c5c523e",
+                To : email,
+                From : "m06011558@gmail.com",
+                Subject : subject,
+                Body : message
+            }).then(
+              message => alert(message)
+            );
 
-            $.ajax({
-                contentType: 'application/json',
-                dataType: 'json',
-                url: "http://ec2-65-0-29-248.ap-south-1.compute.amazonaws.com:5000/api/sendmsg",
-                type: "POST",
-                data: JSON.stringify({
-                    name: name,
-                    email: email,
-                    subject: subject,
-                    message: message
-                }),
-               caches:false,
-                success: function () {
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-success')
-                            .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                            .append('</div>');
-                    $('#contactForm').trigger("reset");
-                },
-                error: function () {
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
-                    $('#success > .alert-danger').append('</div>');
-                    $('#contactForm').trigger("reset");
-                },
-                complete: function () {
-                    setTimeout(function () {
-                        $this.prop("disabled", false);
-                    }, 1000);
-                }
-            });
+            // $.ajax({
+            //     contentType: 'application/json',
+            //     dataType: 'json',
+            //     url: "http://ec2-65-0-29-248.ap-south-1.compute.amazonaws.com:5000/api/sendmsg",
+            //     type: "POST",
+            //     data: JSON.stringify({
+            //         name: name,
+            //         email: email,
+            //         subject: subject,
+            //         message: message
+            //     }),
+            //    caches:false,
+            //     success: function () {
+            //         $('#success').html("<div class='alert alert-success'>");
+            //         $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            //                 .append("</button>");
+            //         $('#success > .alert-success')
+            //                 .append("<strong>Your message has been sent. </strong>");
+            //         $('#success > .alert-success')
+            //                 .append('</div>');
+            //         $('#contactForm').trigger("reset");
+            //     },
+            //     error: function () {
+            //         $('#success').html("<div class='alert alert-danger'>");
+            //         $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            //                 .append("</button>");
+            //         $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
+            //         $('#success > .alert-danger').append('</div>');
+            //         $('#contactForm').trigger("reset");
+            //     },
+            //     complete: function () {
+            //         setTimeout(function () {
+            //             $this.prop("disabled", false);
+            //         }, 1000);
+            //     }
+            // });
         },
         filter: function () {
             return $(this).is(":visible");
